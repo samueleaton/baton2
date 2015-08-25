e712e1843a5da7491fc1ba3c1f407218bff2e22f
Sam Eaton <sameaton11@gmail.com>
2015-08-25 11:20:12 -0600

    adds async option

--- fang.js
+++ fang.js
@@ -1,9 +1,42 @@
 var fang = (function(){
-	
-	// "use strict";
+	"use strict";
 
-	// Initiates Fang Series
+	// Fang Core Methods
 	var _fang = {
+		// Initiates Fang Series
+		init: function () {
+			var args = [];
+			for(var i = 0, ii = arguments.length; i < ii; i++){
+				args.push(arguments[i]);
+			}
+			console.log("this",this)
+			this.utils.i = -1;
+			return this.next.apply(null, args);
+		},
+
+		// Initiates Async Fang Series
+		initAsync: function() {
+			var args = [];
+			var callback;
+			for(var i = 0, ii = arguments.length; i < ii; i++){
+				if (i < ii - 1) args.push(arguments[i]);
+				else {
+					if (typeof arguments[i] !== "function"){
+						callback = null;
+						console.warn("No callback given for fang.initAsync");
+					}
+				}
+			}
+
+			this.utils.queue.forEach(function(func){
+				if(typeof func === "function") func();
+			})
+			console.log("queue",this.utils.queue)
+			console.log("this",this);
+			this.utils.i = -1;
+			return this.next.apply(null, args);
+		},
+
 		// Adds Function to Fang Series
 		then: function(_callback){
 			if(typeof _callback === "function") _fang.utils.queue.push(_callback);
@@ -26,7 +59,17 @@ var fang = (function(){
 			}
 		},
 
+		push: function(callback){
+			if(typeof callback === "function") this.then(callback);
+			else console.error("passed a non-function to fang");
+		},
+
 		utils: {
+			config: {
+				async: false,
+				maxParallel: 10,
+				asyncComplete: null
+			},
 			// the queue that will hold all of the function in the series
 			queue:[], 
 
@@ -34,27 +77,38 @@ var fang = (function(){
 			i:-1 
 		}
 	};
-	
 
-	return function(){
 
-		var newFang = Object.create(_fang);
-		
+
+	function newFang() {
+		var newFangObject = Object.create(_fang);
+
 		for(var i = 0, ii = arguments.length; i < ii; i++){
-			if(typeof arguments[i] === "function") newFang.then(arguments[i]);
+			if(typeof arguments[i] === "function") newFangObject.then(arguments[i]);
 		}
 
-		function run(){
-			var args = [];
-			for(var i = 0, ii = arguments.length; i < ii; i++){
-				args.push(arguments[i]);
+		return {
+			next: function() {
+				newFangObject.next.apply(newFangObject, arguments);
+			},
+			init: function() {
+				newFangObject.init.apply(newFangObject, arguments);
+			},
+			initAsync: function() {
+				newFangObject.initAsync.apply(newFangObject, arguments);
+			},
+			push: function() {
+				newFangObject.push.apply(newFangObject, arguments);
+			},
+			config: function(object) {
+				if(!object) return;
+				newFangObject.utils.async = object.async || false;
+				newFangObject.utils.maxParallel = object.maxParallel || 10;
+				newFangObject.utils.maxParallel = object.asyncComplete || null;
 			}
-			newFang.utils.i = -1;
-			return newFang.next.apply(null, args);
-		}
+		};
+		// return newFangObject;
+	}
 
-		// run.append = newFang.then;
-
-		return run;
-	};
+	return newFang;
 })();
